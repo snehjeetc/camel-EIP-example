4042103844f5df2a5998ae30f2e8e25a823d41ca
snehjeetc <csnehjeet12@gmail.com>
2024-06-24 19:13:40 +0530

    uncommented

--- src/main/java/com/eipexample/eip_camel/camelroutes/CamelRouteConfiguration.java
+++ src/main/java/com/eipexample/eip_camel/camelroutes/CamelRouteConfiguration.java
@@ -16,25 +16,25 @@ public class CamelRouteConfiguration extends RouteBuilder {
         * i.) context based route starts
         */
         System.out.println("Starting routes"); 
-        // try{
-        // from("file://D:/ApacheCamelStarterProjects/ApacheCamelRepoVSCode/eip-camel/orders")
-        // .routeId("fileInId")
-        // .to("kafka://orders?brokers=localhost:9092");
+        try{
+        from("file://D:/ApacheCamelStarterProjects/ApacheCamelRepoVSCode/eip-camel/orders")
+        .routeId("fileInId")
+        .to("kafka://orders?brokers=localhost:9092");
 
      
-        // from("kafka://orders?brokers=localhost:9092")
-        // .log("${body}")
-        // .split(xpath("/orders/order"))
-        // .choice()
-        //     .when(xpath("//order/type/text() = 'vegetable'"))
-        //         .to("activemq:queue:vegetable")
-        //     .when(xpath("//order/type/text() = 'fruits'"))
-        //         .to("activemq:queue:fruits")
-        //     .otherwise()
-        //         .to("kafka://product?brokers=localhost:9092"); 
-        // }catch(Exception ex){ 
-        //     System.out.println("ex in making route" + ex.getMessage()); 
-        // }
+        from("kafka://orders?brokers=localhost:9092")
+        .log("${body}")
+        .split(xpath("/orders/order"))
+        .choice()
+            .when(xpath("//order/type/text() = 'vegetable'"))
+                .to("activemq:queue:vegetable")
+            .when(xpath("//order/type/text() = 'fruits'"))
+                .to("activemq:queue:fruits")
+            .otherwise()
+                .to("kafka://product?brokers=localhost:9092"); 
+        }catch(Exception ex){ 
+            System.out.println("ex in making route" + ex.getMessage()); 
+        }
         /**
          * i.) Context based route ends
          */
